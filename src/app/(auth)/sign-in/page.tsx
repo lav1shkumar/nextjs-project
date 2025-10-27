0020c3cc6f8b792d415db1739809b7215b586c0f
Lavish <lavishkumar@outlook.in>
2025-10-27 14:17:14 +0000

    Updated sign-in page with shadcn ui and react hook form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,37 +1,40 @@
 "use client";
+import { GalleryVerticalEnd } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
+import { signInSchema } from "@/schemas/signInSchema";
 import { useEffect,useState } from "react";
-
 import { signIn,useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 
-
-
-
-const loginSchema = z.object({
-  username: z
-    .string()
-    .min(3, "Username must be at least 3 characters long")
-    .regex(/^[a-zA-Z0-9_]+$/, "Username can only contain letters, numbers, and underscores"),
-  password: z
-    .string()
-    .min(3, "Password must be at least 6 characters long"),
-});
-
-export default function LoginForm() {
+export default function LoginPage() {
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm({
-    resolver: zodResolver(loginSchema), 
+    resolver: zodResolver(signInSchema), 
   });
 
   const [error, setError] = useState("");
@@ -39,8 +42,8 @@ export default function LoginForm() {
   const searchParams = useSearchParams();
   const { data: session, status } = useSession();
   
-  const onSubmit = async (data: z.infer<typeof loginSchema>) => {
-    const identifier = data.username
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    const identifier = data.identifier
     const password = data.password
 
     const res = await signIn("credentials", {
@@ -52,63 +55,102 @@ export default function LoginForm() {
 
     if (res?.error) {
         setError(res.error);
-        console.log(error)
       } else {
         router.push("/");
       }
   };
 
-  
   return (
-    <div className="flex flex-col justify-center items-center w-screen h-screen" >
-      <h2 className="text-5xl font-bold m-5 text-center">Sign in</h2>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col items-center justify-center"
-          onFocus={()=>setError("")}
-        >
-          <div className="flex flex-col items-center justify-center gap-1 max-w-60">
-            <Label className="self-start" htmlFor="username">Username</Label>
-            <Input
-              id="username"
-              type="text"
-              className="mt-1 h-10 w-64"
-              placeholder="Enter username"
-              {...register("username")}
-            />
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
           </div>
-          {errors.username && (
-              <p className="text-red-500 text-sm self-start">
-                {errors.username.message}
-              </p>
-            )}
-          <div className="flex flex-col items-center justify-center gap-1 max-w-60">
-            <Label className="self-start mt-2" htmlFor="password">Password</Label>
-            <Input
-              id="password"
-              type="password"
-              className="mt-1 h-10 w-64"
-              placeholder="Enter password"
-              {...register("password")}
-            />
-          </div>
-          {errors.password && (
-              <p className="text-red-500 text-sm self-start">
-                {errors.password.message}
-              </p>
-            )}
-          
-          <Button variant="outline" size="lg"  type="submit" className="mt-5">
-            Sign In
-          </Button>
-
-          { (
-              <p className="text-red-500 text-sm m-2">
-                {error}
-              </p>
-            )}
-
-        </form>
+          Example
+        </a>
+          <div className="flex flex-col gap-6" >
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Welcome back</CardTitle>
+          <CardDescription>
+            Login with your Apple or Google account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)} onFocus={()=>{setError("")}}>
+            <FieldGroup>
+              <Field>
+                <Button variant="outline" type="button">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path
+                      d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
+                      fill="currentColor"
+                    />
+                  </svg>
+                  Login with Apple
+                </Button>
+                <Button variant="outline" type="button">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path
+                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                  Login with Google
+                </Button>
+              </Field>
+              <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+                Or continue with
+              </FieldSeparator>
+              <Field>
+                <FieldLabel htmlFor="username">Username</FieldLabel>
+                <Input
+                  id="username"
+                  type="text"
+                  placeholder="m@example.com"
+                  {...register("identifier")}
+                />
+                {errors.identifier && (
+                  <p className="text-red-500 text-sm">
+                    {errors.identifier.message}
+                  </p>
+                )}
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <a
+                    href="#"
+                    className="ml-auto text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required {...register("password")}/>
+                {errors.password && (
+                  <p className="text-red-500 text-sm">
+                    {errors.password.message}
+                  </p>
+                )}
+              </Field>
+              <Field>
+                <p className="text-red-500 text-sm text-center">{error}</p>
+                <Button type="submit">Login</Button>
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account? <a href="#">Sign up</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+      <FieldDescription className="px-6 text-center">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </FieldDescription>
     </div>
-  );
+      </div>
+    </div>
+  )
 }
--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -2,6 +2,6 @@ import {z} from 'zod';
 
 
 export const signInSchema = z.object({
-    identifier: z.string(),
-    password: z.string(),
+    identifier: z.string().min(3, { message: "Username must be at least 3 characters" }).regex(/^[a-zA-Z0-9_]+$/, "Username can only contain letters, numbers, and underscores"),
+    password: z.string().min(5, { message: "Password must be at least 6 characters"}),
 })
